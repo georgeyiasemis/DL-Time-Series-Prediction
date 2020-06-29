924bea607631804b388e6caefb19394ea0341b00
Yiasemis, George <george.yiasemis19@imperial.ac.uk>
2020-06-29 15:04:41 +0100

    Replace preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,18 +1,43 @@
 import pandas as pd
-# import matplotlib.pyplot as plt
-# import matplotlib
 import numpy as np
 import datetime
+import sys
+sys.path.insert(0, '../')
 
-def read_data_from_csv(*paths):
-    assert len(paths) in [1, 2]
-    data = pd.read_csv(path[0])
-    prices = pd.read_csv(path[1]) if len(path) == 2
+def read_data_from_csv(path: str):
 
-    return data, prices
+    data = pd.read_csv(path)
 
+    return data
+
+def create_categorical(df, which_columns, one_hot=True):
+    '''
+    Returns a data frame df and replaces with one-hot encodings the specified columns
+    in which_columns
+    '''
+    df = df.copy()
+    columns = list(df.columns)
+    assert set(which_columns).issubset(set(columns))
+    if one_hot:
+        Y = []
+        for column in which_columns:
+            Y.append(pd.get_dummies(df[column], prefix=column))
+            columns.remove(column)
+        df = df[columns]
+        for y in Y:
+            df = pd.concat([df, y], axis=1, sort=False)
+        return df
+    else:
+        for column in which_columns:
+            N = sorted(list(df[column].unique()))
+            y = {val:int(key) for (key,val) in enumerate(N)}
+            new_col = []
+            for d in df[column]:
+                new_col.append(y[d])
+            df[column] = new_col
+        return df
 
 
 if __name__ == '__main__':
-    data_path = 
-    read_data_from_csv('1','2')
+    data_path = "C:\Users\GeorgeYiasemis\OneDrive - Tickmill Ltd\Töölaud\Data\APPL_sentiment.csv"
+    print(read_data_from_csv(data_path))
